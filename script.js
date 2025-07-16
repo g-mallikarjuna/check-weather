76eb806b6ea56a2004f92d75c49eca6a895d5350
Arjun <mallikarjuna44g@gmail.com>
2025-07-16 10:52:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const getcity = document.getElementById('cityName');
+const button = document.getElementById('checkweather');
+const cityname = document.getElementById('cityname');
+const citytemp = document.getElementById('citytemperature'); 
+const citycond = document.getElementById('citycondition');
+const cityhumidity = document.getElementById('cityhumidity');
+
+
+button.addEventListener('click', () => {
+    const city = getcity.value;
+    const apiKey = 'b70bf6aaffd146e1a68132254251107';
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+    fetch(url).then(res => res.json()).then(data => {
+        cityname.textContent = `City: ${data.location.name}`; 
+        citytemp.textContent = `Temperature: ${data.current.temp_c}°C`;
+        citycond.textContent = `Condition:${data.current.condition.text}`;
+        cityhumidity.textContent = `Humidity:${data.current.humidity}`;
+    }).catch(error => {
+        console.log("Error Fetching ");
+    })
+});
